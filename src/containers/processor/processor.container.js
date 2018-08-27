1d518d03b64f6b095ea4f3dcaec35d715da591ac
Yazid Mehenni <yazid129@gmail.com>
2018-08-27 14:42:32 -0400

    setup component env vars

---
+++ src/containers/processor/processor.container.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { WOW_API, LOCALE, APIKEY } from '../../env/env.js';
+
+export class Processor extends Component {
+  state = {
+    guild: 'Baewatch',
+    realm: "Mal'Ganis"
+  };
+  getRoster = async () => {};
+  render() {
+    <div />;
+  }
+}
---
+++ src/env/env.js
@@ -0,0 +1,5 @@
+const WOW_API = 'https://us.api.battle.net/wow/';
+const LOCALE = 'en_US';
+const APIKEY = '';
+
+export { WOW_API, LOCALE, APIKEY };
