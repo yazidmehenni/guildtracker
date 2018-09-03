bc2b14bd24de5b60bc09aaf2d2cf08efe7c57621
Yazid Mehenni <yazid129@gmail.com>
2018-09-03 12:41:37 -0400

    docker fixes

---
+++ server/env/env.js
@@ -0,0 +1,9 @@
+const WOW_API = 'https://us.api.battle.net/wow';
+const LOCALE = 'en_US';
+const APIKEY = 'akyqx3jzzdm47a66s2pdcrkshf3cspjf';
+
+module.exports = {
+  WOW_API,
+  LOCALE,
+  APIKEY
+};
